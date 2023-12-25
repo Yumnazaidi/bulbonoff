f4a09a5c80334ed54fb3a4b9fc53ead72e582843
yumnazaidi <zaidiyumna192@gmail.com>
2023-12-25 17:23:40 +0500

    Files uploaded

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+function on(){
+    document.getElementById('img').src="on.png";
+
+}
+
+function off(){
+    document.getElementById('img').src="off.png";
+}
+
+// function toggle(){
+//     var img= document.getElementById('img').src;
+//     if(img=="off.png"){
+//         document.getElementById('img').src="on.png";
+//     }
+//     else if(img=="on.png"){
+//         document.getElementById('img').src="off.png";
+//     }
+//     else{
+//         console.log("not working");
+//     }
+// }
+
